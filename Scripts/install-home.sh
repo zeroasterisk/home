9b501fd5a09b875aa53aa48ce31ef90dd85edf84
alan bount <alan@zeroasterisk.com>
2016-01-11 11:49:37 -0500

    fix install-home script for public account

--- Scripts/install-home.sh
+++ Scripts/install-home.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git clone git@github.com:zeroasterisk/home.git
+git clone https://github.com/zeroasterisk/home.git
+git remote add write git@github.com:zeroasterisk/home.git
 mv home/* ./ && mv home/.[a-z]* ./ && rmdir home
 git submodule update --init --recursive
 vim +BundleInstall +qall
