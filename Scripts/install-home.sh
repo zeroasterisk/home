50cc72ce9ccab16a5ad3ff546ac0566018ffd1af
alan bount <alan@zeroasterisk.com>
2016-01-11 11:43:47 -0500

    basic updates

--- Scripts/install-home.sh
+++ Scripts/install-home.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 git clone git@github.com:zeroasterisk/home.git
-mv home/* ./
-mv home/.* ./
-rmdir home
+mv home/* ./ && mv home/.[a-z]* ./ && rmdir home
 git submodule update --init --recursive
 vim +BundleInstall +qall
