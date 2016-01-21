44e1f8ecd7317023a00b894e992300737f9cdcac
alan bount <alan@zeroasterisk.com>
2016-01-21 13:46:06 -0500

    npm global installs

---
+++ Scripts/install-npm-globals.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npm install -g \
+aws-cli \
+gulp-cli \
+grunt-cli \
+watchr \
+jasmine-node \
+jasmine-reporters \
+eslint \
+eslint-plugin-meteor \
+eslint-plugin-angular \
+eslint-plugin-react \
+eslint-plugin-lodash \
+jshint \
+pm2
+
