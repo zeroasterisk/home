6870394c66ae385432643f652dcc387245acdf7b
alan blount <alan@zeroasterisk.com>
2016-02-01 09:32:17 -0500

    Update install-home.sh

--- Scripts/install-home.sh
+++ Scripts/install-home.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/zeroasterisk/home.git
-git remote add write git@github.com:zeroasterisk/home.git
 mv home/* ./ && mv home/.[a-z]* ./ && rmdir home
+git remote add write git@github.com:zeroasterisk/home.git
 git submodule update --init --recursive
 vim +BundleInstall +qall
