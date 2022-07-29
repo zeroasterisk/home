865459ef8b90b7f74d60229cd67fdbeeb8e80fc4
alan bount <alan@zeroasterisk.com>
2022-07-29 11:16:26 -0400

    add zsh theme p10k
    because it is fun

--- Scripts/install-home.sh
+++ Scripts/install-home.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 cd $HOME
 git clone https://github.com/zeroasterisk/home.git dotfiles
-cd dotfiles 
+cd dotfiles
 git submodule update --init --recursive
 
+# other clones which are not submodules
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git oh-my-zsh/custom/themes/powerlevel10k
+
+# run script to setup dirs and symlinks
 ./bin/dotfiles
 
+# setup vim
 vim +BundleInstall +qall
 #zsh
 #chsh -s /bin/zsh
