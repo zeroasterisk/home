57d3eef1da37db83b4f1b8b26bd6ed03fc9a2e92
alan bount <alan@zeroasterisk.com>
2024-09-02 00:02:00 -0400

    minor fixes for install scripts

--- Scripts/install-zsh-theme-powerlevel10k.sh
+++ Scripts/install-zsh-theme-powerlevel10k.sh
@@ -3,5 +3,13 @@
 cd $HOME/dotfiles
 
 git submodule add https://github.com/romkatv/powerlevel10k.git powerlevel10k
+git submodule init
+git submodule update
 
 ln -s $HOME/dotfiles/powerlevel10k $HOME/dotfiles/oh-my-zsh/custom/themes/powerlevel10k
+
+cd powerlevel10k
+git checkout master
+git pull origin master
+cd ..
+
