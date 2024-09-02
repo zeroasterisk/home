ddc961ad9fbb4c271077138ab6e6ec98d682bcec
alan bount <alan@zeroasterisk.com>
2024-09-02 00:12:31 -0400

    powerline config linked in installer

--- Scripts/install-zsh-theme-powerlevel10k.sh
+++ Scripts/install-zsh-theme-powerlevel10k.sh
@@ -8,6 +8,8 @@ git submodule update
 
 ln -s $HOME/dotfiles/powerlevel10k $HOME/dotfiles/oh-my-zsh/custom/themes/powerlevel10k
 
+ln -s $HOME/.p10k.zsh $HOME/dotfiles/zsh/p10k.zsh
+
 cd powerlevel10k
 git checkout master
 git pull origin master
