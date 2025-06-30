c490acc333a44b7fc4b2b3f537533d0c9d67056e
alan bount <alan@zeroasterisk.com>
2025-06-29 23:59:59 -0400

    Revert "Refactor: Streamline Zsh PATH and plugin configuration"
    This reverts commit 037be46c36bda6790a148d410d99579510c21cab.

--- zsh/zsh_path.zsh
+++ zsh/zsh_path.zsh
@@ -1,39 +1,25 @@
 
-path=(
-  $HOME/bin
-  /usr/bin/local
-  /usr/local/bin
-  /usr/local/pgsql/bin
-  /usr/local/php5/bin
-  /opt/local/bin
-  /opt/local/sbin
-  /usr/local/mysql/bin
-  /Applications/Xcode.app/Contents/Developer/usr/bin
-  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
-  $HOME/opt/vert.x-2.1.1/bin
-  /opt/local/lib/percona/bin
-  $HOME/.composer/vendor/bin
-  $HOME/google-cloud-sdk/bin
-  /usr/local/git/bin
-  $HOME/.asdf/bin
-  $HOME/.asdf/shims
-  $HOME/.npm-global/bin
-  $ANDROID_HOME/tools/bin
-  /usr/local/opt/gnu-sed/libexec/gnubin
-  ${HOME}/Code/flutter/bin
-  $HOME/.gem/bin
-  $HOME/.gem/ruby/2.6.0/bin
-  $path
-)
+PATH=$HOME/bin:$PATH
+PATH=/usr/bin/local:/usr/local/bin:$PATH
+PATH=/usr/local/pgsql/bin:/usr/local/php5/bin:/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin:$PATH
+PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:$PATH
+PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+PATH=$PATH:$HOME/opt/vert.x-2.1.1/bin
+#PATH=$PATH:/opt/local/heroku/bin
+PATH=$PATH:/opt/local/lib/percona/bin
+#PATH=$PATH:$HOME/.rvm/bin
+PATH=$PATH:$HOME/.composer/vendor/bin
+PATH=$PATH:$HOME/google-cloud-sdk/bin
+PATH=/opt/local/lib/percona/bin:$PATH
+PATH=/usr/local/git/bin:$PATH
 
 # PATH CLEAUP
 # There's something funky as hell in the PATH before it gets to .zshenv (etc)
 # /bin:/sbin:Vim�UnDo�:/usr/local/git/bin:
 # so this will remove it
-path=(${path:#Vim*UnDo*})
+PATH=`echo $PATH | LANG=C sed -e 's/Vim[^\x00-\x7F]UnDo[^\x00-\x7F]://'`
 
 # Export the PATH
-export PATH=$path
+export PATH=$PATH
 
 MANPATH="/opt/local/share/man:$MANPATH"
-export MANPATH=$MANPATH
