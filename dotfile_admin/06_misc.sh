20c42cfe3882a4594fbe8ebe3525cabdb0a4a220
alan bount <alan@zeroasterisk.com>
2025-08-08 22:59:59 +0000

    feat: Add gemini alias and update scripts
    - Add several aliases for the gemini cli to zshrc.
    - Update dotfile_admin/06_misc.sh to be more robust.
    - Update asdf submodule.

--- dotfile_admin/06_misc.sh
+++ dotfile_admin/06_misc.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 # Misc post install cleanup scripts as needed
 
-cd $HOME
-
 # Oh My Zsh Themes
+p10k_install_path="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-if [[ ! -f ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]]:
+if [[ -d "$p10k_install_path" || -d "powerlevel10k" ]]; then
+  echo "powerlevel10k is already installed or present in the current directory. Skipping clone."
+else
   echo "Install powerlevel10k theme"
-
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10k_install_path"
 fi
 
 # Vim
-
-if [[ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]]:
+if [[ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]]; then
   echo "Install NeoVim vim-plug"
-
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+else
+  echo "NeoVim vim-plug is already installed."
 fi
