318deb3a4167e29bc06554bc6869d2821031b1cf
alan bount <alan@zeroasterisk.com>
2026-05-08 11:39:07 -0400

    refactor(zsh): move Claude settings to local environment file

---
+++ zsh/zsh_env.zsh
@@ -0,0 +1,8 @@
+# Customize env all the time
+
+# Fix for uv to force it to use pypi.org/simple as the default index
+export UV_DEFAULT_INDEX=https://pypi.org/simple
+
+# Sketchy but convenient
+# export GEMINI_API_KEY="..."
+# export GOOGLE_API_KEY="..."
